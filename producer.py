2b9267c1742b183bdb93a9039db201fa4aecbf49
iqbalyarkhan <iqbal.khan@utdallas.edu>
2018-11-11 15:34:04 -0600

    Changes producer to crreectly track tweets

--- producer.py
+++ producer.py
@@ -9,74 +9,24 @@ from kafka import SimpleProducer, KafkaClient
 
 # client = SimpleClient("localhost:9092")
 # producer = SimpleProducer(client)
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
+consumer_key = "O2LeBl8mMctUukKbKZ5qr66jo"
+consumer_secret = "bLTeUJOj1mavg0yH3YRqlf4fLYuA0MzAXQCpZxr2thQ0rWL6y3"
+access_token = "118624642-vHCQWviitNV2P72LbsrTdeY1KYiBKJKzOd97YDUs"
+access_token_secret = "elw6KitjKuwBNOnB1sFYHUXKL8j86TdhNL16KvkgcLBEn"
 #Import the necessary methods from tweepy library
 
 class StdOutListener(StreamListener):
     def on_data(self, data):
-        producer.send_messages("trump", data.encode('utf-8'))
-        # print (data)
+        producer.send_messages("manchester", data.encode('utf-8'))
         return True
     def on_error(self, status):
         print (status)
 
-kafka = KafkaClient("localhost:9092")
-producer = SimpleProducer(kafka)
-l = StdOutListener()
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-stream = Stream(auth, l)
-stream.filter(track="trump")
-
-
-
-#*************** THIS WORKS BY ITSELF *******************#
-#This is a basic listener that just prints received tweets to stdout.
-# class StdOutListener(StreamListener):
-
-#     def on_data(self, data):
-#         print data
-#         return True
-
-#     def on_error(self, status):
-#         print status
-
-
-# if __name__ == '__main__':
-
-#     #This handles Twitter authetification and the connection to Twitter Streaming API
-#     l = StdOutListener()
-#     auth = OAuthHandler(consumer_key, consumer_secret)
-#     auth.set_access_token(access_token, access_token_secret)
-#     stream = Stream(auth, l)
-
-#     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-#     stream.filter(track=['python', 'javascript', 'ruby'])
-#     producer = KafkaProducer(bootstrap_servers='localhost:8080')
-#     topic = "twitter"
-#     producer.send(topic, stream)
-
-#*************** THIS WORKS BY ITSELF *******************#
-# from tweepy.streaming import StreamListener
-# from tweepy import OAuthHandler
-# from tweepy import Stream
-# from kafka import SimpleProducer, KafkaClient
-
-# class StdOutListener(StreamListener):
-#     def on_data(self, data):
-#         producer.send_messages("trump", data.encode('utf-8'))
-#         print (data)
-#         return True
-#     def on_error(self, status):
-#         print (status)
-
-# kafka = KafkaClient("localhost:9092")
-# producer = SimpleProducer(kafka)
-# l = StdOutListener()
-# auth = OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_token, access_secret)
-# stream = Stream(auth, l)
-# stream.filter(track="trump")
+if __name__ == '__main__':
+    kafka = KafkaClient("localhost:9092")
+    producer = SimpleProducer(kafka)
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+    stream.filter(track=['manchester'])
